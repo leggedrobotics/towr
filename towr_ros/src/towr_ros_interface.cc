53ee48862fd86b0dc9bdfe6638bda2fc9f63f35e
Marko Bjelonic <marko-bjelonic@t-online.de>
2021-01-08 14:04:40 +0100

    Minor change.

--- towr_ros/src/towr_ros_interface.cc
+++ towr_ros/src/towr_ros_interface.cc
@@ -256,8 +256,10 @@ TowrRosInterface::SaveOptimizationAsRosbag (const std::string& bag_name,
     grid_map::Position position;
     map.getPosition(*it, position);
 //    map.at("elevation", *it) = 0.4;
-    if (position.x()>0.9)
-      map.at("elevation", *it) = 0.1;
+    if (position.x()>1.7)
+      map.at("elevation", *it) = 0.35;
+    else if (position.x()>0.9)
+      map.at("elevation", *it) = 0.2;
     else
       map.at("elevation", *it) = 0;
   }
